8682caf44e484ad6b4cb23e71e5e1036f23efa4e
erickign <erick.ignacio.t.villegas@intel.com>
2022-02-15 22:31:24 -0800

    updated

--- main-classes.cpp
+++ main-classes.cpp
@@ -11,8 +11,21 @@ class Shape
 {
     public:
         T m_shape;
+        float m_XSpeed, m_YSpeed;
         sf::Text m_text;
-        void setPosition();
+        void setPosition()
+        {
+            // position of m_shape
+            sf::Vector2f previousPosition = m_shape.getPosition();
+            sf::Vector2f moveVector(m_XSpeed, m_YSpeed);
+            sf::Vector2f newPosition = previousPosition + moveVector;
+            m_shape.setPosition(newPosition);
+
+            // position of text
+            // get position of m_shape and put an offset before assigning
+            
+
+        };
 };
 
 int main()
