faaaed54f53ed7f350484d4df0e05c77f1cf0e52
erickign <erick.ignacio.t.villegas@intel.com>
2022-02-15 21:14:30 -0800

    updated

--- main-file-handling.cpp
+++ main-file-handling.cpp
@@ -2,14 +2,13 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <utility>
 
 // https://stackoverflow.com/questions/9435385/split-a-string-using-c11
-std::vector<std::string> split(const std::string &s) {
+std::vector<std::string> split(const std::string &s, char delim) {
   std::stringstream ss(s);
   std::string item;
   std::vector<std::string> elems;
-  char delim;
-  delim = ' ';
   while (std::getline(ss, item, delim)) {
     elems.push_back(item);
   }
@@ -36,22 +35,42 @@ void openAndReadFile()
    std::cout << data << std::endl;
    ifile.close();
 }
+/*
+sample function that will open config file and split using space as 
+delimiter.
+also shows that you can use array indexing on std::vector
+*/
 void openAndReadConfigFile(std::string filename)
 {
    std::string data;
    std::ifstream ifile;
    ifile.open(filename);
    std::cout << "Reading from the file" << std::endl;
+   std::vector<std::string> myvec;
    if (ifile.is_open())
    {
       std::cout << "FILE IS OPEN" << std::endl;
       while (std::getline(ifile, data))
       {
          std::cout << data << std::endl;
-         for (std::string element : split(data))
+         myvec = split(data, ' ');
+         if (myvec[0].compare("Window") == 0) 
          {
-            std::cout << element << std::endl;
-         };
+            std::cout << "Window FOUND!" << std::endl;
+         }
+         if (myvec[0].compare("Font") == 0) 
+         {
+            std::cout << "Font FOUND!" << std::endl;
+         }
+         if (myvec[0].compare("Circle") == 0) 
+         {
+            std::cout << "Circle FOUND!" << std::endl;
+         }
+         if (myvec[0].compare("Rectangle") == 0) 
+         {
+            std::cout << "Rectangle FOUND!" << std::endl;
+         }
+
       }
    }
    ifile.close();
